feefa16d5bae2739048bd0b02f728b77b9da8848
Aditya Jangra <200434215@srudent.georgianc.on.ca>
2020-01-23 21:44:33 -0500

    completed lab3

---
+++ PhoneApplication/Controllers/PhoneController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PhoneApplication.Models;
+
+namespace PhoneApplication.Controllers
+{
+    public class PhoneController : Controller
+    {
+        public IActionResult Index()
+        {
+            var phoneList = new List<Phone>
+            {
+                new Phone {PhoneName = "iPHone 11", Manufacturer="Apple",MSRP="abc",ScreenSize=16, DateReleased= new DateTime(20,02,06)},
+                new Phone {PhoneName = "Samsung S11", Manufacturer="Samsung",MSRP="abc",ScreenSize=14, DateReleased= new DateTime(20,02,06)},
+                new Phone {PhoneName = "Razor", Manufacturer="Razor inc.",MSRP="abc",ScreenSize=17, DateReleased= new DateTime(20,02,05)},
+                new Phone {PhoneName = "Google Phone", Manufacturer="Google",MSRP="abc",ScreenSize=16, DateReleased= new DateTime(20,02,06)},
+            };
+            return View(phoneList);
+        }
+
+
+        }
+}
---
+++ PhoneApplication/Models/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneApplication.Models
+{
+    public class Phone
+    {
+        public Phone()
+        {
+            PhoneName = "";
+            Manufacturer = "";
+            DateReleased = new DateTime();
+        }
+
+        public int PhoneID { get; set; }
+        public string PhoneName { get; set; }
+        public string Manufacturer { get; set; }
+        public string MSRP { get; set; }
+        public int ScreenSize{ get; set; }
+        public DateTime DateReleased { get; set; }
+
+    }
+}
